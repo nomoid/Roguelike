dd2603b73bf9bcb459738ea5ca41b257ec0caa01
Markus Feng <markusfeng@gmail.com>
2018-01-03 15:47:52 -0500

    Added avatar and message displays

--- js_src/game.js
+++ js_src/game.js
@@ -2,11 +2,22 @@ import ROT from 'rot-js';
 import * as U from './util.js';
 
 export let Game = {
-    display: {
-      SPACING: 1.1,
+
+    _DISPLAY_SPACING: 1.1,
+    _display: {
       main: {
         w: 80,
         h: 24,
+        o: null
+      },
+      avatar: {
+        w: 20,
+        h: 24,
+        o: null
+      },
+      message: {
+        w: 100,
+        h: 6,
         o: null
       }
     },
@@ -17,31 +28,49 @@ export let Game = {
       console.log("using random seed" + this._randomSeed);
       ROT.RNG.setSeed(this._randomSeed);
 
-      this.display.main.o = new ROT.Display({
-        width: this.display.main.w,
-        height: this.display.main.h,
-        spacing: this.display.SPACING
-      });
+      for(var display_key in this._display){
+        this._display[display_key].o = new ROT.Display({
+          width: this._display[display_key].w,
+          height: this._display[display_key].h,
+          spacing: this._DISPLAY_SPACING
+        });
+      }
 
       U.printTest();
 
     },
 
     getDisplay: function(displayId){
-      if(this.display.hasOwnProperty(displayId)){
-        return this.display[displayId].o;
+      if(this._display.hasOwnProperty(displayId)){
+        return this._display[displayId].o;
       }
       return null;
     },
 
     render: function(){
-      this.renderMain();
+      this.renderDisplayAvatar();
+      this.renderDisplayMain();
+      this.renderDisplayMessage();
     },
 
-    renderMain: function(){
-      let d = this.display.main.o;
-      for(let i = 0; i < 10; i++){
-        d.drawText(5, i + 5, "hello markus");
+    renderDisplayAvatar: function(){
+      let d = this._display.avatar.o;
+      for(let i = 0; i < 20; i++){
+        d.drawText(5, i, "avatar");
+      }
+    },
+
+    renderDisplayMain: function(){
+      let d = this._display.main.o;
+      for(let i = 0; i < 20; i++){
+        d.drawText(5, i, "main");
+      }
+    },
+
+    renderDisplayMessage: function(){
+      let d = this._display.message.o;
+      for(let i = 0; i < 5; i++){
+        d.drawText(5, i, "message");
       }
     }
 };
