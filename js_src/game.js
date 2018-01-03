f032b53bce8725fd2528be0e344b79cbf90f9d7f
Markus Feng <markusfeng@gmail.com>
2018-01-03 15:24:16 -0500

    Added ROT console

--- js_src/game.js
+++ js_src/game.js
@@ -1,12 +1,44 @@
-import 'babel-polyfill';
-import {utilAlert} from './util.js';
 import ROT from 'rot-js';
+import * as U from './util.js';
 
-console.dir(ROT);
+export let Game = {
+    display: {
+      SPACING: 1.1,
+      main: {
+        w: 80,
+        h: 24,
+        o: null
+      }
+    },
 
-document.write("ROT support status: "+ROT.isSupported()+"<br/>");
+    init: function(){
+      this._randomSeed = 5 + Math.floor(Math.random() * 100000);
+      //this._randomSeed = 76250;
+      console.log("using random seed" + this._randomSeed);
+      ROT.RNG.setSeed(this._randomSeed);
 
-let name = "Bob", time = "today";
-console.log(`Hello ${name}, how are you ${time}?`);
+      this.display.main.o = new ROT.Display({
+        width: this.display.main.w,
+        height: this.display.main.h,
+        spacing: this.display.SPACING
+      });
+    },
 
-utilAlert();
+    getDisplay: function(displayId){
+      if(this.display.hasOwnProperty(displayId)){
+        return this.display[displayId].o;
+      }
+      return null;
+    },
+
+    render: function(){
+      this.renderMain();
+    },
+
+    renderMain: function(){
+      let d = this.display.main.o;
+      for(let i = 0; i < 10; i++){
+        d.drawText(5, i + 5, "hello world");
+      }
+    }
+};
