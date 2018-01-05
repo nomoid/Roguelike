4cf3865398b2b33e1f845ce8498f9b27c8b8388c
Markus Feng <markusfeng@gmail.com>
2018-01-05 14:04:44 -0500

    Added fancy startup text

--- js_src/ui_mode.js
+++ js_src/ui_mode.js
@@ -1,3 +1,4 @@
+import * as U from './util.js';
 
 class UIMode{
   constructor(){
@@ -25,7 +26,15 @@ export class StartupMode extends UIMode{
   }
 
   render(display){
-    display.drawText(2, 2, "Welcome to PickledPopcorn")
+    U.drawTextWithSpaces(display, 2, 2, "  _____ _      _    _          _ _____                                 ");
+    U.drawTextWithSpaces(display, 2, 3, " |  __ (_)    | |  | |        | |  __ \\                                ");
+    U.drawTextWithSpaces(display, 2, 4, " | |__) |  ___| | _| | ___  __| | |__) |__  _ __   ___ ___  _ __ _ __  ");
+    U.drawTextWithSpaces(display, 2, 5, " |  ___/ |/ __| |/ / |/ _ \\/ _` |  ___/ _ \\| '_ \\ / __/ _ \\| '__| '_ \\ ");
+    U.drawTextWithSpaces(display, 2, 6, " | |   | | (__|   <| |  __/ (_| | |  | (_) | |_) | (_| (_) | |  | | | |");
+    U.drawTextWithSpaces(display, 2, 7, " |_|   |_|\\___|_|\\_\\_|\\___|\\__,_|_|   \\___/| .__/ \\___\\___/|_|  |_| |_|");
+    U.drawTextWithSpaces(display, 2, 8, "                                           | |                         ");
+    U.drawTextWithSpaces(display, 2, 9, "                                           |_|                         ");
+
   }
 }
 
--- js_src/util.js
+++ js_src/util.js
@@ -5,3 +5,17 @@ export function utilAlert(){
 export function printTest(){
   console.log("Some important piece of information that needs to be logged");
 }
+
+export function drawTextWithSpaces(display, x, y, text){
+  if(text.charAt(0) == ' '){
+    for(let i = 1; i < text.length; i++){
+      if(text.charAt(i) == ' '){
+        x++;
+      }
+      else{
+        break;
+      }
+    }
+  }
+  display.drawText(x, y, text);
+}
