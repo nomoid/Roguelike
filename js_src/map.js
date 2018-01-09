bc04a26927a74993a6f8054140ec3be3c9896e8e
Markus Feng <markusfeng@gmail.com>
2018-01-09 13:49:32 -0500

    Added basic map implementation

---
+++ js_src/map.js
@@ -0,0 +1,22 @@
+import {TILES} from './tile.js';
+import {init2DArray} from './util.js';
+
+class Map{
+  constructor(xdim, ydim){
+    this.xdim = xdim || 1;
+    this.ydim = ydim || 1;
+    this.tileGrid = init2DArray(this.xdim, this.ydim, TILES.NULLTILE);
+  }
+
+  render(display, camera_x, camera_y){
+    let cx = 0;
+    let cy = 0;
+    for(let xi = 0; xi < this.xdim; xi++){
+      for(let yi = 0; yi < this.ydim; yi++){
+        this.tileGrid[xi][yi].render(display, cx, cy);
+        cy++;
+      }
+      cx++;
+    }
+  }
+}
--- js_src/tile.js
+++ js_src/tile.js
@@ -1,14 +1,14 @@
 import {DisplaySymbol} from './display_symbol.js';
 
-export class Tile extends DisplaySymbol{
+class Tile extends DisplaySymbol{
   constructor(name, chr, fg, bg){
     super(chr, fg, bg);
     this.name = name;
   }
 }
 
-let TILES = {
-  NULLTILE = new Tile('nulltile', ' '),
-  WALL = new Wall('wall', '#'),
-  FLOOR = new Floor('floor', '.')
+export let TILES = {
+  NULLTILE: new Tile('nulltile', '#'),
+  WALL: new Wall('wall', '#'),
+  FLOOR: new Floor('floor', '.')
 }
