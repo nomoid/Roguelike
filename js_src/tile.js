64a5750fb30943d3008da579975bc97b489678f3
Markus Feng <markusfeng@gmail.com>
2018-01-09 13:39:05 -0500

    Added several types of default tiles

--- js_src/tile.js
+++ js_src/tile.js
@@ -1,8 +1,14 @@
 import {DisplaySymbol} from './display_symbol.js';
 
 export class Tile extends DisplaySymbol{
-  constructor(chr, fg, bg, name){
+  constructor(name, chr, fg, bg){
     super(chr, fg, bg);
     this.name = name;
   }
+}
+
+let TILES = {
+  NULLTILE = new Tile('nulltile', ' '),
+  WALL = new Wall('wall', '#'),
+  FLOOR = new Floor('floor', '.')
 }
